ecbb5eaad2d217532084138991c7331d6f68f300
jarooda <jaluwibowoaji@gmail.com>
2021-03-04 14:05:46 +0700

    feat(controller:task): Move tasks_controller to controllers/api/v1 feat(controller:home): Add home_controller feat(model:task): Validate for due_date feat(model:user): Add has_many :tasks feat(routes): Add name_spaces "api" and "v1"

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates :due_date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :tasks
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
+    end
+  end
+
+  root to: "home#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
